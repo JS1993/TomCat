26f2731f0483dac5f288eea7835966b248f1a012
susu <28025920@qq.com>
2016-04-03 21:47:18 +0800

    重构，只需要23行代码

--- TomCat/TomCat/ViewController.m
+++ TomCat/TomCat/ViewController.m
@@ -5,46 +5,19 @@
 //  Created by  江苏 on 16/4/3.
 //  Copyright © 2016年 jiangsu. All rights reserved.
 //
-
 #import "ViewController.h"
-
 @interface ViewController ()
 @property (strong, nonatomic) IBOutlet UIImageView *tom;
-
 @end
-
 @implementation ViewController
+-(void)viewDidLoad{
+    [super viewDidLoad];
+}
 - (IBAction)Action:(UIButton *)sender {
-    switch (sender.tag) {
-        case 0:
-            [self setAnimations:@"eat" AndInteger:40];
-            break;
-        case 1:
-            [self setAnimations:@"drink" AndInteger:81];
-            break;
-        case 2:
-            [self setAnimations:@"cymbal" AndInteger:13];
-            break;
-        case 3:
-            [self setAnimations:@"fart" AndInteger:28];
-            break;
-        case 4:
-            [self setAnimations:@"pie" AndInteger:24];
-            break;
-        case 5:
-            [self setAnimations:@"scratch" AndInteger:55];
-            break;
-        case 6:
-            [self setAnimations:@"knockout" AndInteger:81];
-            break;
-        default:
-            break;
-    }
+    [self setAnimations:sender.currentTitle AndInteger:sender.tag];
 }
 -(void)setAnimations:(NSString* )name AndInteger:(int)integer{
-    if ([self.tom isAnimating]) {
-        return;
-    }
+    if ([self.tom isAnimating]) return;
     NSMutableArray* arr=[NSMutableArray array];
     for (int i=0; i<integer; i++) {
         NSString* imageName=[NSString stringWithFormat:@"%@_%02d.jpg",name,i];
@@ -57,10 +30,6 @@
     self.tom.animationDuration=arr.count*0.075;
     [self.tom startAnimating];
     //动画结束后，清理内存
-//    [self performSelector:@selector(clearup) withObject:nil afterDelay:self.tom.animationDuration];
     [self.tom performSelector:@selector(setAnimationImages:) withObject:nil afterDelay:self.tom.animationDuration];
 }
-//-(void)clearup{
-//    self.tom.animationImages=nil;
-//}
 @end
