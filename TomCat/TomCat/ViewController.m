6ac0cbd8b3a0751532bbfeab5cc2d5d98d11c249
susu <28025920@qq.com>
2016-04-03 20:59:21 +0800

    增加了动画

--- TomCat/TomCat/ViewController.m
+++ TomCat/TomCat/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *tom;
 
 @end
 
@@ -16,12 +17,95 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+}
+- (IBAction)Action:(UIButton *)sender {
+    switch (sender.tag) {
+        case 0:
+        {
+            NSMutableArray* arr=[NSMutableArray array];
+            for (int i=0; i<40; i++) {
+                NSString* imageName=[NSString stringWithFormat:@"eat_%02d.jpg",i];
+                UIImage* image=[UIImage imageNamed:imageName];
+                [arr addObject:image];
+            }
+            self.tom.animationImages=arr;
+            self.tom.animationDuration=arr.count*0.075;
+            [self.tom startAnimating];
+        }
+            break;
+        case 1:
+        {
+            NSMutableArray* arr=[NSMutableArray array];
+            for (int i=0; i<81; i++) {
+                NSString* imageName=[NSString stringWithFormat:@"drink_%02d.jpg",i];
+                UIImage* image=[UIImage imageNamed:imageName];
+                [arr addObject:image];
+            }
+            self.tom.animationImages=arr;
+            self.tom.animationDuration=arr.count*0.075;
+            [self.tom startAnimating];
+        }
+            break;
+        case 2:
+        {
+            NSMutableArray* arr=[NSMutableArray array];
+            for (int i=0; i<13; i++) {
+                NSString* imageName=[NSString stringWithFormat:@"cymbal_%02d.jpg",i];
+                UIImage* image=[UIImage imageNamed:imageName];
+                [arr addObject:image];
+            }
+            self.tom.animationImages=arr;
+            self.tom.animationDuration=arr.count*0.075;
+            [self.tom startAnimating];
+        }
+            break;
+        case 3:
+        {
+            NSMutableArray* arr=[NSMutableArray array];
+            for (int i=0; i<27; i++) {
+                NSString* imageName=[NSString stringWithFormat:@"fart_%02d.jpg",i];
+                UIImage* image=[UIImage imageNamed:imageName];
+                [arr addObject:image];
+            }
+            self.tom.animationImages=arr;
+            self.tom.animationDuration=arr.count*0.075;
+            [self.tom startAnimating];
+        }
+            break;
+        case 4:
+        {
+            NSMutableArray* arr=[NSMutableArray array];
+            for (int i=0; i<24; i++) {
+                NSString* imageName=[NSString stringWithFormat:@"pie_%02d.jpg",i];
+                UIImage* image=[UIImage imageNamed:imageName];
+                [arr addObject:image];
+            }
+            self.tom.animationImages=arr;
+            self.tom.animationDuration=arr.count*0.075;
+            [self.tom startAnimating];
+        }
+            break;
+        case 5:
+        {
+            NSMutableArray* arr=[NSMutableArray array];
+            for (int i=0; i<56; i++) {
+                NSString* imageName=[NSString stringWithFormat:@"scratch_%02d.jpg",i];
+                UIImage* image=[UIImage imageNamed:imageName];
+                [arr addObject:image];
+            }
+            self.tom.animationImages=arr;
+            self.tom.animationDuration=arr.count*0.075;
+            [self.tom startAnimating];
+        }
+            break;
+        default:
+            break;
+    }
 }
 
 @end
